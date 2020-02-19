ea0095f0e8e53aaebe1eeee91636ac5129ddcf9e
Matt Kennett <matt@mattkennett.com>
2020-02-18 22:12:48 -0500

    2020-02-18 state of code

--- wordy_game_app/urls.py
+++ wordy_game_app/urls.py
@@ -4,4 +4,5 @@ from wordy_game_app.views import *
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('register', Register.as_view(), name='register'),
 ]
--- wordy_game_app/views.py
+++ wordy_game_app/views.py
@@ -1,7 +1,7 @@
-import os
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+
+import wordy_game_app.models as wordy_models
 
 
 class Index(View):
@@ -21,3 +21,54 @@ class Index(View):
             }
 
         return render(request, 'wordy_game_app/index.html', context)
+
+class Register(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'wordy_game_app/register.html')
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username', None)
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+        password_verify = request.POST.get('password_verify', None)
+        favorite_color = request.POST.get('favorite_color', None)
+
+        if not username or not email or not password or not password_verify:
+            context = {
+                'error': 'Must Pass In Username, Email, Password, and Password Verify.'
+            }
+            return render(request, 'wordy_game_app/register.html', context=context)
+
+        if not password == password_verify:
+            context = {
+                'error': 'Passwords did not match. Please try again.'
+            }
+            return render(request, 'wordy_game_app/register.html', context=context)
+
+        if not username.isalnum():
+            context = {
+                'error': 'Username must be alphanumeric.'
+            }
+            return render(request, 'wordy_game_app/register.html', context=context)
+
+        # user, created = wordy_models.WordyUser.objects.get_or_create()
+
+        username_exists = wordy_models.WordyUser.objects.all().filter(
+            username=username,
+        ).count()
+
+        if username_exists:
+            context = {
+                'error': 'Username already in use.'
+            }
+            return render(request, 'wordy_game_app/register.html', context=context)
+
+        new_user = wordy_models.WordyUser(
+            username=username,
+            email=email,
+            favorite_color=favorite_color,
+        )
+        new_user.set_password(password)
+        new_user.save()
+
+        return redirect('index')
